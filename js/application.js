32c057c8d51f36717e2c04178cf29ce0063ed9d1
Meg Sandman <margaret.sandman@gmail.com>
2015-01-18 14:13:20 -0800

    add database integration

--- js/application.js
+++ js/application.js
@@ -2,7 +2,19 @@ $(document).ready(function() {
 
   var myFirebaseRef = new Firebase("https://dabble-website.firebaseio.com/");
 
-  $( ".email-submit" ).on( "click", function( event ) {
-    console.log('worked')
+  $( ".email-submit" ).click(function() {
+    var emailAddress = $(".email-form").val()
+    console.log(emailAddress)
+
+    myFirebaseRef.push({
+      email: emailAddress,
+      dateAdded: Date()
+    });
+
+    $(".email-form").val("")
   });
+
+
+
+
 });
